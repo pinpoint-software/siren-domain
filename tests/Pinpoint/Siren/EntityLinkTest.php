fe5b971c23ec2061c437d1fc2fc0e969d25e94fe
Andrew Shell <andrew@andrewshell.org>
2014-06-12 17:56:04 -0500

    Added EntityLinkTest

---
+++ tests/Pinpoint/Siren/EntityLinkTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Pinpoint\Siren;
+
+class EntityLinkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRelAndHrefJson()
+    {
+        $link = new EntityLink();
+        $link->setRelationship(new Relationship('self'));
+        $link->setHref('http://api.x.io/orders/42');
+        $expectedJson = json_encode(
+            array(
+                'rel' => array('self'),
+                'href' => 'http://api.x.io/orders/42',
+            )
+        );
+        $this->assertJsonStringEqualsJsonString($expectedJson, json_encode($link));
+    }
+
+    public function testMissingRelJson()
+    {
+        $this->setExpectedException('Exception');
+        $link = new EntityLink();
+        $link->setHref('http://api.x.io/orders/42');
+        json_encode($link);
+    }
+
+    public function testMissingHrefJson()
+    {
+        $this->setExpectedException('Exception');
+        $link = new EntityLink();
+        $link->setRelationship(new Relationship('self'));
+        json_encode($link);
+    }
+
+    public function testSingleClassJson()
+    {
+        $link = new EntityLink();
+        $link->setRelationship(new Relationship('self'));
+        $link->setHref('http://api.x.io/orders/42');
+        $link->addClass('order');
+        $expectedJson = json_encode(
+            array(
+                'rel' => array('self'),
+                'href' => 'http://api.x.io/orders/42',
+                'class' => array('order'),
+            )
+        );
+        $this->assertJsonStringEqualsJsonString($expectedJson, json_encode($link));
+    }
+
+    public function testDoubleClassJson()
+    {
+        $link = new EntityLink();
+        $link->setRelationship(new Relationship('self'));
+        $link->setHref('http://api.x.io/orders/42');
+        $link->addClass('order');
+        $link->addClass('info');
+        $expectedJson = json_encode(
+            array(
+                'rel' => array('self'),
+                'href' => 'http://api.x.io/orders/42',
+                'class' => array('order', 'info'),
+            )
+        );
+        $this->assertJsonStringEqualsJsonString($expectedJson, json_encode($link));
+    }
+}
